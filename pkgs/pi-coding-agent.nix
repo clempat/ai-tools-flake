6b7d1e273aa75845fa42d8dd8d7ef493f9a0244e
clempat <2178406+clempat@users.noreply.github.com>
2026-06-06 15:20:33 +0200

    chore(pi): update coding agent (#65)

--- pkgs/pi-coding-agent.nix
+++ pkgs/pi-coding-agent.nix
@@ -5,7 +5,8 @@
 }:
 
 let
-  version = "0.70.6";
+  # renovate: datasource=npm depName=@mariozechner/pi-coding-agent
+  version = "0.73.1";
 in
 writeShellScriptBin "pi" ''
   export PATH="${nodejs_22}/bin:$PATH"
